cd6f51079b03d3155a22999de01e32f8186745f4
camryn williams <williams.camryn.e@gmail.com>
2021-04-22 11:08:22 -0400

    completed code along

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  state = {
+    currInSpace: []
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.currInSpace.map(astro => astro.name)}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          currInSpace: data.people
+        })
+      })
+  }
+}
